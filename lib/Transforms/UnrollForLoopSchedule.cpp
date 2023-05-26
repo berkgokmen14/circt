d2bac5339b75b40728e28ab8e0d4df7e8be89bf7
matth2k <mrh259@cornell.edu>
2023-05-25 23:21:11 -0400

    Print dependencies

--- lib/Transforms/UnrollForLoopSchedule.cpp
+++ lib/Transforms/UnrollForLoopSchedule.cpp
@@ -7,12 +7,16 @@
 //===----------------------------------------------------------------------===//
 
 #include "PassDetail.h"
+#include "circt/Analysis/DependenceAnalysis.h"
 #include "circt/Transforms/Passes.h"
+#include "mlir/Dialect/Affine/LoopUtils.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/Operation.h"
 #include "mlir/Pass/Pass.h"
 
 using namespace mlir;
+using namespace mlir::affine;
 
 namespace {
 struct UnrollForLoopSchedule
@@ -22,12 +26,60 @@ struct UnrollForLoopSchedule
   void runOnOperation() override;
 
 private:
+  DenseSet<AffineMapAccessInterface> getMemoryOps(AffineForOp forOp);
 };
 } // namespace
 
+DenseSet<AffineMapAccessInterface>
+UnrollForLoopSchedule::getMemoryOps(AffineForOp forOp) {
+  DenseSet<AffineMapAccessInterface> memOps;
+  auto &bodyRegion = forOp.getLoopBody();
+  // Only affine memory dependencies are supported
+  auto affineLdOps = bodyRegion.getOps<AffineLoadOp>();
+  auto affineStOps = bodyRegion.getOps<AffineStoreOp>();
+  memOps.insert(affineLdOps.begin(), affineLdOps.end());
+  memOps.insert(affineStOps.begin(), affineStOps.end());
+  return memOps;
+}
+
 void UnrollForLoopSchedule::runOnOperation() {
-  llvm::errs() << "UnrollForLoopSchedule\n";
-  auto func = getOperation();
+  SmallVector<SmallVector<AffineForOp>> loopNests;
+  for (auto rootForLoop : getOperation().getOps<AffineForOp>()) {
+    SmallVector<AffineForOp> loopNest;
+    loopNest.push_back(rootForLoop);
+    getPerfectlyNestedLoops(loopNest, rootForLoop);
+    loopNests.push_back(loopNest);
+  }
+
+  auto memDepAnalysis =
+      getAnalysis<circt::analysis::MemoryDependenceAnalysis>();
+
+  for (auto &loopNest : loopNests) {
+    auto innerLoop = loopNest.back();
+
+    if (!innerLoop.getLoopBody().getOps<memref::LoadOp>().empty() ||
+        !innerLoop.getLoopBody().getOps<memref::StoreOp>().empty())
+      return signalPassFailure();
+
+    auto memOps = getMemoryOps(innerLoop);
+    for (auto dest : memOps) {
+      auto inboudDeps = memDepAnalysis.getDependences(dest);
+      for (const auto &inboudDep : inboudDeps) {
+        if (llvm::isa<AffineMapAccessInterface>(*inboudDep.source) &&
+            memOps.contains(
+                llvm::cast<AffineMapAccessInterface>(*inboudDep.source)) &&
+            inboudDep.dependenceType != DependenceResult::ResultEnum::Failure) {
+          if (inboudDep.dependenceType ==
+              DependenceResult::ResultEnum::HasDependence)
+            llvm::errs() << "there is a ";
+          else
+            llvm::errs() << "there is NOT a ";
+          llvm::errs() << "dep from " << *inboudDep.source << " to " << dest
+                       << " with distance " << inboudDep.dependenceType << "\n";
+        }
+      }
+    }
+  }
 }
 
 namespace circt {
